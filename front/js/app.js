955660d1b2ac52eb8e569aa2ab60c9179c90dc26
valentinCro <valentin.crochemore1@etu.univ-rouen.fr>
2016-06-25 21:40:10 +0200

    feat() : add tendance

--- front/js/app.js
+++ front/js/app.js
@@ -55,6 +55,8 @@
                 var trajet = this;
                 trajet.vitesseMax = 50;
                 trajet.tendance = 0;
+                trajet.goodDriveKm = 0;
+                trajet.bagDriveKm = 0;
                 trajet.latitude = null;
                 trajet.longitude = null;
                 if ("geolocation" in navigator) {
@@ -62,15 +64,27 @@
                     trajet.started = false;
 
                     trajet.geo_success = function (position) {
-                        trajet.latitude = position.coords.latitude;
-                        trajet.longitude = position.coords.longitude;
-                        if (position.coords.speed === null) {
-                            trajet.speed = 0;
-                            if (trajet.speed > trajet.vitesseMax) {
-                                trajet.tendance += 0.1;
+                        if (trajet.latitude !== position.coords.latitude
+                            || trajet.longitude !== position.coords.longitude) {
+                            trajet.oldLatitude = trajet.latitude;
+                            trajet.latitude = position.coords.latitude;
+                            trajet.oldLongitude = trajet.longitude;
+                            trajet.longitude = position.coords.longitude;
+
+                            if (position.coords.speed === null) {
+                                trajet.speed = 0;
+                                var dist = trajet.calculDistance(trajet.oldLatitude, trajet.latitude,
+                                trajet.oldLongitude, trajet.longitude);
+                                if (trajet.speed > trajet.vitesseMax) {
+                                    trajet.tendance -= 0.1;
+                                    trajet.bagDriveKm += dist;
+                                } else {
+                                    trajet.tendance += 0.01;
+                                    trajet.goodDriveKm += dist;
+                                }
+                            } else {
+                                trajet.speed = position.coords.speed / 1000;
                             }
-                        } else {
-                            trajet.speed = position.coords.speed / 1000;
                         }
                     };
 
@@ -86,6 +100,18 @@
 
                     navigator.geolocation.watchPosition(trajet.geo_success, trajet.geo_error, trajet.geo_options);
 
+                    trajet.isUp = function() {
+                        return trajet.tendance > 0;
+                    }
+
+                    trajet.isDown = function() {
+                        return trajet.tendance < 0;
+                    }
+
+                    trajet.isNeutral = function() {
+                        return trajet.tendance === 0;
+                    }
+
                     trajet.getCurrentLatitude = function() {
                         return trajet.latitude;
                     };
@@ -96,6 +122,16 @@
 
                     trajet.getSpeed = function() {
                         return trajet.speed;
+                    };
+
+                    trajet.calculDistance = function(lat1, lat2, lon1, lon2){
+                        var delta1 = lat1.toRadians();
+                        var delta2 = lat2.toRadians();
+                        var lambda = (lon2-lon1).toRadians();
+                        var R = 6371e3; // gives d in metres
+                        var d = Math.acos( Math.sin(delta1)*Math.sin(delta2) + Math.cos(delta1)*Math.cos(delta2)
+                                * Math.cos(lambda) ) * R;
+                        return d / 1000;
                     };
                 } else {
                     /* geolocation IS NOT available */
